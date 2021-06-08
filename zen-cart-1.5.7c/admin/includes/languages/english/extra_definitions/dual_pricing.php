f6d9adb9c85774188e165975dd7106123ad909e8
Steve <4391026+torvista@users.noreply.github.com>
2021-06-08 22:11:55 +0200

    fix typo, remove duplicate, rationalise texts into one file to avoid touching core files.

---
+++ zen-cart-1.5.7c/admin/includes/languages/english/extra_definitions/dual_pricing.php
@@ -0,0 +1,20 @@
+<?php
+// plugin Dual Pricing
+// admin texts - english
+
+//attributes_controller.php
+define('TABLE_HEADING_OPT_PRICE_W', ' Wholesale Price');
+//customers.php
+define('ENTRY_WHOLESALE', 'Wholesale:');
+define('ENTRY_WHOLESALE_PRICING_LEVEL', 'Wholesale Pricing Level: ');
+define('NOTES_WHOLESALE_PRICING_LEVEL', '<br>For RETAIL customers this should be 0, for wholesale customers please enter the price band.');
+define('TABLE_HEADING_WHOLESALE', 'Retail/Wholesale');
+//product/collect_info.php
+define('TEXT_WHOLESALE_PRICE_COMMENT', '<br>If there is no wholesale price, set this field to 0');
+define('TEXT_WHOLESALE_PRICE_NET', 'Wholesale Price (Net)');
+//products_price_manager.php
+define('TEXT_PRICE_W', 'Price Wholesale (net)');
+define('TEXT_PRODUCTS_DISCOUNT_PRICE_W', 'Wholesale Discount Value');
+define('TEXT_PRODUCTS_DISCOUNT_PRICE_EACH_W', 'Wholesale Calculate Price:');
+define('TEXT_PRODUCTS_DISCOUNT_PRICE_EXTENDED_W', 'Wholesale Extended Price:');
+?>
